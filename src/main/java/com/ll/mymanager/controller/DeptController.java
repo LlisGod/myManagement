9a82c3f71e33cc98684ec407b8601d85e5ff4f29
LlisGod <1658853457@qq.com>
2026-03-29 11:52:28 +0800

    feat(): 添加了logback配置文件，改为使用log和@Slf4j注解记录日志

--- src/main/java/com/ll/mymanager/controller/DeptController.java
+++ src/main/java/com/ll/mymanager/controller/DeptController.java
@@ -3,11 +3,13 @@ package com.ll.mymanager.controller;
 import com.ll.mymanager.pojo.ApiResponse;
 import com.ll.mymanager.pojo.Dept;
 import com.ll.mymanager.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController()
 @RequestMapping("/depts")
 public class DeptController {
@@ -17,7 +19,8 @@ public class DeptController {
 
     @GetMapping
     public ApiResponse<List<Dept>> List(){
-        System.out.println("查询部门信息");
+//        System.out.println("查询部门信息");
+        log.info("查询部门信息");
         List<Dept> deptList = deptService.findAll();
          return ApiResponse.success(deptList);
     }
@@ -27,7 +30,8 @@ public class DeptController {
      */
     @DeleteMapping
     public ApiResponse<String> Delete(@RequestParam("id") Integer deptId){
-        System.out.println("删除部门信息"+deptId);
+//        System.out.println("删除部门信息"+deptId);
+        log.info("删除部门信息"+deptId);
         deptService.delete(deptId);
         return ApiResponse.success("删除成功");
     }
@@ -37,7 +41,8 @@ public class DeptController {
      */
     @PostMapping
     public ApiResponse<String> Add(@RequestBody Dept dept){
-        System.out.println("新增部门信息"+dept);
+//        System.out.println("新增部门信息"+dept);
+        log.info("新增部门信息"+dept);
         deptService.add(dept);
         return ApiResponse.success("新增成功");
     }
@@ -46,6 +51,7 @@ public class DeptController {
      */
     @GetMapping("/{id}")
     public ApiResponse<Dept> FindDeptById(@PathVariable Integer id){
+        log.info("查询部门信息"+id);
         Dept dept=deptService.findDeptById(id);
         return ApiResponse.success(dept);
     }
@@ -55,7 +61,8 @@ public class DeptController {
      */
     @PutMapping
     public ApiResponse<String> Update(@RequestBody Dept dept){
-        System.out.println("修改部门信息"+dept);
+//        System.out.println("修改部门信息"+dept);
+        log.info("修改部门信息"+dept);
         deptService.update(dept);
         return ApiResponse.success();
     }
--- src/main/java/com/ll/mymanager/controller/EmpController.java
+++ src/main/java/com/ll/mymanager/controller/EmpController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.ll.mymanager.pojo.ApiResponse;
 import com.ll.mymanager.pojo.Emp;
 import com.ll.mymanager.service.EmpService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/emps")
 public class EmpController {
@@ -20,9 +22,16 @@ public class EmpController {
     @Autowired
     private EmpService empService;
 
+    /*
+     * 分页查询员工信息
+     * @param pageNum
+     * @param pageSize
+     * @param
+     */
     @GetMapping
     public ApiResponse<IPage<Emp>> page(@RequestParam Integer pageNum, @RequestParam Integer pageSize){
-        System.out.println("查询员工信息");
+//        System.out.println("查询员工信息");
+        log.info("查询员工信息 第{}页,{}每页",pageNum,pageSize);
         IPage<Emp> emps = empService.page(pageNum, pageSize);
         return ApiResponse.success(emps);
     }
